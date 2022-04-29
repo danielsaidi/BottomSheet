b85f386db0a8e197285cd1c00ebf240e49b50edc
Daniel Saidi <daniel.saidi@gmail.com>
2022-04-29 14:10:41 +0200

    Add DocC plugin dependency

--- Package.swift
+++ Package.swift
@@ -14,7 +14,9 @@ let package = Package(
             name: "BottomSheet",
             targets: ["BottomSheet"])
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+    ],
     targets: [
         .target(
             name: "BottomSheet",
